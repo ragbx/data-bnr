9764b6c370701ced9118697b4ce98ac2649b0619
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-09 12:23:47 +0100

    sets OAI

--- oai_bnr_sets.sh
+++ oai_bnr_sets.sh
@@ -1,1 +1,2 @@
-catmandu convert OAI --url http://oai.bn-r.fr/oai.php --listSets 1 --fix "copy_field(setDescription.0.description.0, set_description);retain(setSpec, setName, set_description)" to XLSX > results/oai_sets_bnr_description.xlsx
+date=$(date '+%Y%m%d')
+catmandu convert OAI --url http://oai.bn-r.fr/oai.php --listSets 1 --fix "copy_field(setDescription.0.description.0, set_description);retain(setSpec, setName, set_description)" to XLSX > results/oai_sets_bnr_description_$date.xlsx
