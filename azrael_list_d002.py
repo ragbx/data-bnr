9935ecd5d6982237c61dea74a1aae9c1bc2c4686
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-16 08:46:49 +0100

    MaJ

---
+++ azrael_list_d002.py
@@ -0,0 +1,15 @@
+from bnr.azrael import Azrael2list
+
+"""
+Prestation CADN 2020
+DDE_2020_I
+MED_MS
+5992 copiés dans BNR_VERIF le 05/01/2021
+"""
+
+root_path = 'E:/'
+az2list = Azrael2list(root_path=root_path, code_disk='D002')
+az2list.list_files()
+az2list.export_list(chunksize=1000)
+
+az2list.process_lists(result_size=1000)
