1e1f9597030198a0435034a4e2fbe760c389d002
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-18 14:37:21 +0100

    MaJ

--- azrael_list_d001.py
+++ azrael_list_d001.py
@@ -2,7 +2,7 @@ from bnr.azrael import Azrael2list
 
 root_path = '/home/kibini/bnr/'
 az2list = Azrael2list(root_path=root_path, code_disk='D001')
-#az2list.list_files()
-#az2list.export_list(chunksize=1000)
+az2list.list_files()
+az2list.export_list(chunksize=1000)
 
-az2list.process_lists(result_size=1000)
+#az2list.process_lists(result_size=1000)
