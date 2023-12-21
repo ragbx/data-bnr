2ae611435a86e43ddc92c43694c5ba612a827162
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-21 10:45:03 +0100

    MaJ

--- azrael_list_d003.py
+++ azrael_list_d003.py
@@ -11,4 +11,4 @@ az2list = Azrael2list(root_path=root_path, code_disk='d003')
 
 #az2list.export_list(chunksize=1000)
 
-az2list.process_lists(result_size=1000, exif=True)
+az2list.process_lists(result_size=1000, exif=False)
