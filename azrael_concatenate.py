e356cbb3d4d19edf87ce0a8bc8cb7a833c7c1835
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-22 16:24:59 +0100

    MaJ

--- azrael_concatenate.py
+++ azrael_concatenate.py
@@ -5,15 +5,16 @@ import time
 
 today = time.strftime('%Y%m%d')
 
-root_path = join("data", "bnr_d001_tmp")
+root_path = join("data")
 
 r = []
 for dir_path, dirs, files in walk(root_path):
     for file in sorted(files):
         if file[-6:] == 'csv.gz':
-            print(file)
-            file_path = join(dir_path, file)
-            df_file = pd.read_csv(file_path)
-            r.extend(df_file.to_dict(orient='records'))
+            if file[4:8] == 'd008':
+                print(file)
+                file_path = join(dir_path, file)
+                df_file = pd.read_csv(file_path)
+                r.extend(df_file.to_dict(orient='records'))
 df = pd.DataFrame(r)
-df.to_csv(join("data", "bnr_d001_TMP_20231218.csv.gz"), index=False)
+df.to_csv(join("data", "bnr_d008_20231221.csv.gz"), index=False)
