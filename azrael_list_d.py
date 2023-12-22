90a88316aadae26caab5203fde05bae8ea441353
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-22 16:02:41 +0100

    MaJ

--- azrael_list_d.py
+++ azrael_list_d.py
@@ -1,11 +1,11 @@
 from bnr.azrael import Azrael2list
 
 """
-DDE_2008I
+
 """
 
 root_path = 'E:/'
-az2list = Azrael2list(root_path=root_path, code_disk='d003')
+az2list = Azrael2list(root_path=root_path, code_disk='d008')
 
 #az2list.list_files()
 
--- azrael_list_d001.py
+++
@@ -1,8 +0,0 @@
-from bnr.azrael import Azrael2list
-
-root_path = '/home/kibini/bnr/'
-az2list = Azrael2list(root_path=root_path, code_disk='d001')
-az2list.list_files()
-az2list.export_list(chunksize=1000)
-
-#az2list.process_lists(result_size=1000)
