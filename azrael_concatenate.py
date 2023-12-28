f79f664108e76b9b4af95734682b458060193ee8
François Pichenot <fpichenot@ville-roubaix.fr>
2023-12-28 11:02:17 +0100

    MaJ

--- azrael_concatenate.py
+++ azrael_concatenate.py
@@ -5,16 +5,19 @@ import time
 
 today = time.strftime('%Y%m%d')
 
-root_path = join("data")
+root_path = join("/media", "fpichenot", "ragbx", "data-bnr-trans")
+prefix = "d016"
+date = "20231227"
+
 
 r = []
 for dir_path, dirs, files in walk(root_path):
     for file in sorted(files):
         if file[-6:] == 'csv.gz':
-            if file[4:8] == 'd008':
+            if (file[4:8] == prefix or file[8:12] == prefix):
                 print(file)
                 file_path = join(dir_path, file)
                 df_file = pd.read_csv(file_path)
                 r.extend(df_file.to_dict(orient='records'))
 df = pd.DataFrame(r)
-df.to_csv(join("data", "bnr_d008_20231221.csv.gz"), index=False)
+df.to_csv(join("data", f"bnr_{prefix}_{date}_pbMD5.csv.gz"), index=False)
---
+++ azrael_dde_diff.py
@@ -0,0 +1,32 @@
+from os.path import join, isfile
+from os import listdir
+import pandas as pd
+root_path = "data"
+
+# on créée deux dataframes à comparer
+
+azrael_prefix = "d001"
+dde_prefix = [f"d{n :03d}" for n in range(2, 18)]
+
+files = [f for f in listdir(root_path) if f[-6:] == 'csv.gz']
+dde_df = pd.DataFrame()
+for file in files:
+    if file[4:8] == azrael_prefix:
+        azrael_df = pd.read_csv(join(root_path, file))
+        azrael_df['prefix'] = file[4:8]
+    elif file[4:8] in dde_prefix:
+        dde_df_part = pd.read_csv(join(root_path, file))
+        dde_df_part['prefix'] = file[4:8]
+        dde_df = pd.concat([dde_df, dde_df_part])
+
+# fichiers pour lesquels on a les md5
+dde_df_md5ok = dde_df[~dde_df['md5'].isna()]
+dde_df_md5ok_notinazrael = dde_df_md5ok[~dde_df_md5ok['md5'].isin(azrael_df['md5'])]
+dde_df_md5ok_notinazrael.to_csv(join(root_path, "dde_df_md5ok_notinazrael.csv.gz"), index=False)
+print(len(dde_df_md5ok_notinazrael))
+
+# fichiers pour lesquels on n'a pas les md5
+dde_df_md5ko = dde_df[dde_df['md5'].isna()]
+dde_df_md5ko_notinazrael = dde_df_md5ko[~dde_df_md5ko['name'].isin(azrael_df['name'])]
+dde_df_md5ko_notinazrael.to_csv(join(root_path, "dde_df_md5ko_notinazrael.csv.gz"), index=False)
+print(len(dde_df_md5ko_notinazrael))
